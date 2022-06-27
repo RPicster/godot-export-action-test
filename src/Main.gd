f64b442f384840664044fad9ca3a5c83c984bcf0
Raffaele Picca <picster@pixelgod.net>
2022-06-27 18:58:55 +0200

    Tested and working on Mac

--- src/Main.gd
+++ src/Main.gd
@@ -1,15 +1,44 @@
 extends ColorRect
 
+var commands = ["mac_copy", "mac_paste", "mac_cut", "mac_select_all"]
+var commands_keys = [KEY_C, KEY_V, KEY_X, KEY_A]
+
+
+func _ready():
+	add_mac_actions()
+
+
+func add_mac_actions():
+	for i in commands.size():
+		var new_event = InputEventKey.new()
+		new_event.scancode = commands_keys[i]
+		new_event.meta = true
+		new_event.pressed = true
+		InputMap.action_add_event(commands[i], new_event)
+	for i in commands.size():
+		var new_event = InputEventKey.new()
+		new_event.scancode = commands_keys[i]
+		new_event.command = true
+		new_event.pressed = true
+		InputMap.action_add_event(commands[i], new_event)
+
+
+
 func _input(event):
 	if event is InputEventKey and event.pressed:
 		if event.is_action_pressed("mac_copy"):
+			show_debug("simulate: mac copy")
 			simulate_input(KEY_C)
 		if event.is_action_pressed("mac_paste"):
+			show_debug("simulate: mac paste")
 			simulate_input(KEY_V)
 		if event.is_action_pressed("mac_cut"):
+			show_debug("simulate: mac cut")
 			simulate_input(KEY_X)
 		if event.is_action_pressed("mac_select_all"):
+			show_debug("simulate: mac select all")
 			simulate_input(KEY_A)
+
 
 func simulate_input(new_scancode):
 	var input_event = InputEventKey.new()
@@ -20,3 +49,7 @@ func simulate_input(new_scancode):
 	
 	input_event.pressed = false
 	Input.parse_input_event(input_event)
+
+
+func show_debug(text):
+	$DebugLabel.text = text
